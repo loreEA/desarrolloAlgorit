fae6a2e223eb240418f6b0c93d9f75768bb000ef
lorena <lorenaancaten73@gmail.com>
2022-04-07 14:52:28 -0300

    ult cambios

--- Avion.java
+++ Avion.java
@@ -1,3 +1,60 @@
 public class Avion {
+    //Atributos de la clase Avion
+    private String identificador;
+    private String modelo;
+    private int cantAsientos;
+    private double kmRecorrid;
+    private double velocidProm;
+
+    //Constructores
+    public Avion(String unIdentif){
+        identificador = unIdentif;
+        modelo="";
+        cantAsientos = 0;
+        kmRecorrid = 0;
+        velocidProm = 0;
+    }
+    public Avion(String unIdentif,String unModelo,int unaCantAsient,double kilRecorrid,double unaVeloc){
+        identificador = unIdentif;
+        modelo = unModelo;
+        cantAsientos = unaCantAsient;
+        kmRecorrid= kilRecorrid;
+        velocidProm = unaVeloc;
+    }
+    
+    //Métodos observadores
+    /*Estos métodos obtendrán un valor del tipo texto, entero o real, dependiendo de lo que contengan 
+    cada uno de los atributos*/
+    public String getIdentificador(){
+        return this.identificador;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public int getCantidAsientos(){
+        return cantAsientos;
+    }
+    public double getKilomRecorrid(){
+        return kmRecorrid;
+    }
+    public double getVelocidadProm(){
+        return velocidProm;
+    }
+    //Métodos modificadores
+    //Estos métodos modicarán, según el tipo de valor, a ciertos atributos. Excep. al identif y modelo
+    public void setCantAsientos(int cantidAsient){
+        this.cantAsientos = cantidAsient;
+    }
+    public void setKilomRecorrid(double kilomRecorrid){
+        this.kmRecorrid = kilomRecorrid;
+    }
+    public void setVelocidadProm(double unaVelocidProm){
+        this.velocidProm = unaVelocidProm;
+    }
+    //Métodos propias del tipo
+    //Este método compará los promedios de las velocidades de dos aviones y devolverá un valor logico
+    public boolean esMayorEnVeloc(int unaVelocProm){
+        return this.velocidProm >= velocidProm;
+    }
     
 }
--- TestAvion.java
+++ TestAvion.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class TestAvion {
-    public static void name() {
-        
+    public static void main(String[] args) {
+        int cantAsient;
+        double promVeloc, kmRecorridos;
+        String modeloAv, identificad;
+        Avion unAvion;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese por favor el identificador del avion");
+        identificad = entrada.nextLine();
+        unAvion = new Avion(identificad);
+        System.out.println("Por favor ingrese el modelo del avion "+unAvion.getIdentificador());
+        modeloAv = entrada.nextLine();
+        System.out.println("Por favor ingrese la cantidad de asientos del modelo de Avion "+unAvion.getModelo());
+        cantAsient = entrada.nextInt();
+        System.out.println("Por favor ingrese la cantidad de km recorridos del avion "+unAvion.getIdentificador());
+        kmRecorridos = entrada.nextDouble();
+        System.out.println("Por favor ingrese el promedio de velocidad del avion "+unAvion.getIdentificador());
+        promVeloc = entrada.nextDouble();
+        unAvion = new Avion(identificad,modeloAv,cantAsient,kmRecorridos,promVeloc);
     }
 }
