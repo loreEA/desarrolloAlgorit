48265a9f856b3b18ff02695cf6d0942b035f63eb
lorena <lorenaancaten73@gmail.com>
2022-04-08 00:34:10 -0300

    nuevo

--- TestAvion.java
+++ TestAvion.java
@@ -1,36 +1,37 @@
 import java.util.Scanner;
 public class TestAvion {
     public static void main(String[] args) {
-        int cantAsient;
-        double promVeloc, kmRecorridos;
-        String modeloAv, identificad;
-        Avion unAvion;
-        boolean esMayor;
+        Avion unAvion, unAvion2;
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Ingrese por favor el identificador del avion");
-        identificad = entrada.nextLine();
-        unAvion = new Avion(identificad);
-        System.out.println("Por favor ingrese el modelo del avion "+unAvion.getIdentificador());
-        modeloAv = entrada.nextLine();
-        System.out.println("Por favor ingrese la cantidad de asientos del modelo de Avion "+unAvion.getIdentificador());
-        cantAsient = entrada.nextInt();
-        System.out.println("Por favor ingrese la cantidad de km recorridos del avion "+unAvion.getIdentificador());
-        kmRecorridos = entrada.nextDouble();
-        System.out.println("Por favor ingrese el promedio de velocidad del avion "+unAvion.getIdentificador());
-        promVeloc = entrada.nextDouble();
-        unAvion = new Avion(identificad,modeloAv,cantAsient,kmRecorridos,promVeloc);
-        System.out.println(unAvion.getModelo());
-        System.out.println(unAvion.getCantidAsientos());
-        System.out.println(unAvion.getKilomRecorrid());
-        System.out.println(unAvion.getVelocidadProm());
-        System.out.println("Ingrese el promedio de la velocidad de otro avion y verificaremos si es mayor al del avion "+unAvion.getModelo());
-        promVeloc = entrada.nextDouble();
-        esMayor = unAvion.esMayorEnVeloc(promVeloc);
-        if(esMayor){
-            System.out.println("Es mayor");
-        }
-        else{
-            System.out.println("No es mayor");
-        }
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println("                                   INGRESAR LOS DATOS DE DOS AVIONES");
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        unAvion = cargarAvion();
+        unAvion2 = cargarAvion();
+
     }
+    /*Este método creára un objeto del tipo Avion y lo retornará*/
+    public static Avion cargarAvion(){
+        Scanner sc = new Scanner(System.in);
+        String id, modelo;
+        Avion avion;
+        int cantidAsient;
+        double kmRecorrid, promedVeloc;
+        System.out.println("Ingrese por favor el identificador del avion");
+        id = sc.nextLine();
+        avion = new Avion(id);
+        System.out.println("Por favor ingrese el modelo del avion "+avion.getIdentificador());
+        modelo = sc.nextLine();
+        System.out.println("Por favor ingrese la cantidad de asientos del modelo de Avion "+avion.getIdentificador());
+        cantidAsient = sc.nextInt();
+        System.out.println("Por favor ingrese la cantidad de km recorridos del avion "+avion.getIdentificador());
+        kmRecorrid = sc.nextDouble();
+        System.out.println("Por favor ingrese el promedio de velocidad del avion "+avion.getIdentificador());
+        promedVeloc = sc.nextDouble();
+        avion = new Avion(id,modelo,cantidAsient,kmRecorrid,promedVeloc);
+        return avion;
+    }
+
+    
+
 }
