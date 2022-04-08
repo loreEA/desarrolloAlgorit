6756504f1393775645c9404d8b65954a279daaa8
lorena <lorenaancaten73@gmail.com>
2022-04-08 01:26:01 -0300

    ultima modificacion a la clase Auto y su test

--- TestAvion.java
+++ TestAvion.java
@@ -4,10 +4,14 @@ public class TestAvion {
         Avion unAvion, unAvion2;
         Scanner entrada = new Scanner(System.in);
         System.out.println("-----------------------------------------------------------------------------------------------");
-        System.out.println("                                   INGRESAR LOS DATOS DE DOS AVIONES");
+        System.out.println("                                      CARGAR PRIMER AVION");
         System.out.println("-----------------------------------------------------------------------------------------------");
         unAvion = cargarAvion();
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println("                                     CARGAR SEGUNDO AVION");
+        System.out.println("-----------------------------------------------------------------------------------------------");
         unAvion2 = cargarAvion();
+        menu(unAvion, unAvion2);
 
     }
     /*Este método creára un objeto del tipo Avion y lo retornará*/
@@ -32,6 +36,52 @@ public class TestAvion {
         return avion;
     }
 
-    
+    public static void menu(Avion primerAvion, Avion segundAvion) {
+        int opcion;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println("                                        MENU");
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println("Porfavor ingrese algún valor entero según las siguientes opciones:");
+        System.out.println("1: Mostrar todos los datos del primer avión");
+        System.out.println("2: Mostrar todos los datos del segundo avión");
+        System.out.println("3: Modificar la cantidad de asientos, km recorridos y promedio de velocidad del primer avión");
+        System.out.println("4: Modificar la cantidad de asientos, km recorridos y promedio de velocidad del segundo avión");
+        System.out.println("Cualquier otro valor: Salida del programa");
+        
+        do {
+            System.out.println("                                    OPCIÓN");
+            opcion = scan.nextInt();
+            switch(opcion){
+                case 1: mostrarAvion(primerAvion);break;
+                case 2: mostrarAvion(segundAvion);break;
+                case 3: modificarAvion(primerAvion);break;
+                case 4: modificarAvion(segundAvion);break;
+                default: opcion = 0;
+            }
+        } while (opcion>0 && opcion<5);
+    }
 
+    public static void mostrarAvion(Avion unAvion) {
+        System.out.println("El identificador del Avión es: "+unAvion.getIdentificador());
+        System.out.println("El modelo del Avión "+unAvion.getIdentificador()+" es: "+unAvion.getModelo());
+        System.out.println("La cantidad de asientos de "+unAvion.getIdentificador()+" es: "+unAvion.getCantidAsientos());
+        System.out.println("La cantidad de km recorridos del Avión "+unAvion.getIdentificador()+" es: "+unAvion.getKilomRecorrid());
+        System.out.println("La velocidad promedio del Avión "+unAvion.getModelo()+" es: "+unAvion.getVelocidadProm());
+    }
+
+    public static void modificarAvion(Avion unAvion){
+        int cantAsients;
+        double promedioVeloc, kilomRecorridos;
+        Scanner scann = new Scanner(System.in);
+        System.out.println("Ingresar cuántos asientos tiene ahora el Avión "+unAvion.getIdentificador());
+        cantAsients = scann.nextInt();
+        System.out.println("Ingresar cuántos km tiene recorrido ahora el Avión "+unAvion.getIdentificador());
+        kilomRecorridos = scann.nextDouble();
+        System.out.println("Ingresar el promedio de velocidad que tiene el Avión en la actualidad ");
+        promedioVeloc = scann.nextDouble();
+        unAvion.setCantAsientos(cantAsients);
+        unAvion.setKilomRecorrid(kilomRecorridos);
+        unAvion.setVelocidadProm(promedioVeloc);
+    }
 }
