3dd68e7bd1b57f18334d44aa585e52f865a36c6b
lorena <lorenaancaten73@gmail.com>
2022-04-25 16:49:15 -0300

    new changes

--- recursividad/ejer1.java
+++ recursividad/ejer1.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
 public class ejer1 {
-    
+    public static void main(String[] args) {
+        int n, sumatoria;
+        n = cargarNum();
+        sumatoria = fun(n);
+        System.out.println(sumatoria);
+
+    }
+    public static int cargarNum(){
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Porfavor ingrese un número entero.");
+        num = sc.nextInt();
+        if(num<1){
+            do{
+                System.out.println("Error. Ingrese un valor valido.");
+                num = sc.nextInt();
+            }while(num<1);
+        }
+        sc.close();
+        return num;
+    }
+    public static int fun(int n){
+        int aux=0;
+        for(int i=n; i>=0; i--)
+        {
+            aux = aux + i;
+        }
+        return aux;
+    }
 }
