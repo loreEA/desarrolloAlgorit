2aa5cd7330abec80cd1ae716f1a6e04f23ecca3e
lorena <lorenaancaten73@gmail.com>
2022-04-27 15:54:25 -0300

    nuevos camb

--- recursividad/ej4.java
+++ recursividad/ej4.java
@@ -7,18 +7,17 @@ public class ej4 {
         System.out.println("CARGAR UN NÚMERO");
         System.out.println("Porfavor ingrese un número");
         numero = sc.nextInt();
-        sonIguales= sonIguals(numero/10, numero%10);
+        sonIguales= sonIguals(numero/10, numero%10);/*Aquí enseguida extraemos el último dígito para directamente comparar en el método */
         System.out.println("VERIFICADOR");
         System.out.println("¿Los dígitos del número son iguales? "+sonIguales);
-        sc.close();
     }
     public static boolean sonIguals(int n, int m){
         boolean aux;
-        if(n<10 && n/10==m){
+        if(n<10 && n==m){ /*Esto verifica que estemos parados en el primer dígito y lo comparamos para ver si es igual al último dígito extraído */
             aux = true;
         }
         else{
-            if(n%10 == m){
+            if(n%10 == m){ /*Este extrae y verifica que el último dígito del nuevo número sea igual al último dígito extraído */
                 m = n%10;
                 aux = sonIguals(n/10, m);
             }
