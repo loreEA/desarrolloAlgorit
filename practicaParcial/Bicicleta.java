81ad0b9fe6265f5eb104be6d4b79cb576e33bfd2
lorena <lorenaancaten73@gmail.com>
2022-05-03 15:11:49 -0300

    new

---
+++ practicaParcial/Bicicleta.java
@@ -0,0 +1,10 @@
+public class Bicicleta {
+    //Atributos
+    private int numSerie;
+    private String marca;
+    private int cantCambios;
+    private char tipoDeBici;
+    private double precioVenta;
+    private boolean segDeFabrica;
+    //Constructores
+}
