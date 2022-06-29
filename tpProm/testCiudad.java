10e49c04b4d3213bc31746f440d8673638475493
loreEA <103203347+loreEA@users.noreply.github.com>
2022-06-29 11:48:31 -0300

    Update testCiudad.java

--- tpProm/testCiudad.java
+++ tpProm/testCiudad.java
@@ -1,21 +1,17 @@
 public class testCiudad{
     public static void main(String[] args) {
-        String ciudad = "Piedra del Aguila",ciudad2;
-        ciudad2 = abreviaturaCiudad(ciudad, 0);
-        System.out.println(ciudad2);
     }
     public static void menu() {
-        
     }
-    private static String abreviaturaCiudad(String ciudad,int i) {
+    private static String abreviatCiudad(String ciudad,int i) {
         String abreviatura;
         boolean esVoc;
         if (i<ciudad.length()) {
             esVoc = esVocal(ciudad.charAt(i));
             if (ciudad.charAt(i) == ' ' || esVoc) {
-                abreviatura = abreviaturaCiudad(ciudad, i+1);
+                abreviatura = abreviatCiudad(ciudad, i+1);
             } else {
-                abreviatura = ciudad.charAt(i)+ abreviaturaCiudad(ciudad, i+1);
+                abreviatura = ciudad.charAt(i)+ abreviatCiudad(ciudad, i+1);
             }
         } else {
             abreviatura = "";
@@ -26,5 +22,41 @@ public class testCiudad{
         String vocales = "aeiouAEIOU";
         return vocales.indexOf(letra)>-1;
     }
-
+    public static void ordSeleccion(Ciudad[] ciudades,int tipoOrd) {
+        int longitud = ciudades.length, i, j, iMin;
+        if (tipoOrd==1) {
+            for (i = 0; i < longitud - 1; i++) {
+                iMin = i;
+                for (j = i+1; j < longitud; j++) {
+                    if (ciudades[j].compareTo(ciudades[iMin])>-1) {
+                       iMin = j;
+                       intercambio(ciudades,i,iMin);
+                    }
+                }
+            }
+        } else {
+            for (i = 0; i < longitud - 1; i++) {
+                iMin = i;
+                for (j = i+1; j < longitud; j++) {
+                    if (ciudades[j].compareTo(ciudades[iMin])<0) {
+                       iMin = j;
+                       intercambio(ciudades,i,iMin);
+                    }
+                }
+            }
+        }  
+    }
+    public static void ordMergeSort(Ciudad[] ciudades, int tipoOrd) {
+        if (tipoOrd==3) {
+            
+        } else {
+            
+        }
+    }
+    public static void intercambio(Ciudad[] ciudades,int i,int j){
+        String nombre;
+        nombre = ciudades[i].getNombre();
+        ciudades[i].setNombre(ciudades[j].getNombre());
+        ciudades[j].setNombre(nombre);
+    }
 }
